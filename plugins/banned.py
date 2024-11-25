372c573ac350fc3c75aad5a1367e55e10d5dd610
Kokopw <kmelon175@gmail.com>
2024-11-25 23:09:11 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -39,7 +39,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('Support', url=f'https://t.me/JISSHU_SUPPORT')
+        InlineKeyboardButton('Support', url=f'https://t.me/CharlieSupportGroup')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
